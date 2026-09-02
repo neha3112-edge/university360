af1bc9388828027dc4d70a766e1e91325ba59cdf
Edge Technosoft pvt. ltd. <support@edgetechnosoft.com>
2026-09-02 17:28:00 +0530

    Make Changes 4

--- iiitb.php
+++ iiitb.php
@@ -410,7 +410,7 @@
         </div>
     </section>
 
-    
+
     <!-- Programmes / Course Cards Section -->
     <section class="amity-programmes-section" id="programmes">
         <div class="container">
@@ -613,56 +613,38 @@
     <section class="amity-approvals-section" id="approvals">
         <div class="container">
             <h2 class="approvals-main-heading text-center">Accreditation and Approvals of IIIT Bangalore</h2>
-            <div class="row mt-4">
+            <div class="row mt-4 custom_accreditation_row">
                 <div class="col-lg-4 col-md-6 col-sm-6">
                     <div class="amity-approval-card">
-                        <div class="approval-icon"><img src="images/ugc-1.webp" alt="UGC Approved"></div>
-                        <h4>UGC Approved</h4>
-                        <p>Ensures the university is recognized by the University Grants Commission (UGC) and its
-                            degrees are valid across India.</p>
+                        <div class="approval-icon"><img src="images/naac-a-iiit.webp" alt="UGC Approved"></div>
+                        <h4>NAAC A+</h4>
+                        <p>The IIIT Bangalore certificate courses have an A+ grade, which proves that the institute
+                            offers quality learning outcomes for professionals.</p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-6">
                     <div class="amity-approval-card">
-                        <div class="approval-icon"><img src="images/approvals-badge.webp" alt="UGC-DEB Recognized">
+                        <div class="approval-icon"><img src="images/ugc-1.webp" alt="UGC-DEB Recognized">
                         </div>
-                        <h4>UGC-DEB Recognized</h4>
-                        <p>Confirms that the university is authorized to offer online and distance education programs as
-                            per UGC-DEB guidelines.</p>
+                        <h4>UGC</h4>
+                        <p>This recognition confirms the institute's credibility and supports trust in online
+                            credentials.</p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-6">
                     <div class="amity-approval-card">
-                        <div class="approval-icon"><img src="images/naac-a.webp" alt="Globally Accredited"></div>
-                        <h4>Globally Accredited</h4>
-                        <p>International Institute of Information Technology Bangalore is internationally recognized,
-                            ensuring the DBA
-                            degree meets global
-                            academic and professional standards.</p>
+                        <div class="approval-icon"><img src="images/aicte-1.webp" alt="Globally Accredited"></div>
+                        <h4>AICTE</h4>
+                        <p>It makes sure that the course is industry-aligned with curriculum standards, technical
+                            rigour, and value.</p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-6">
                     <div class="amity-approval-card">
-                        <div class="approval-icon"><img src="images/aicte-1.webp" alt="AICTE Approved"></div>
-                        <h4>AICTE Approved</h4>
-                        <p>Indicates that technical and management programs comply with the quality standards set by
-                            AICTE.</p>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="amity-approval-card">
-                        <div class="approval-icon"><img src="images/nirf-2.webp" alt="NIRF Ranked"></div>
-                        <h4>NIRF Ranked</h4>
-                        <p>Reflects the university's performance and ranking based on teaching, research, graduation
-                            outcomes, and overall excellence.</p>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="amity-approval-card">
-                        <div class="approval-icon"><img src="images/wes.webp" alt="AIU Recognized"></div>
-                        <h4>AIU Member</h4>
-                        <p>Ensures the university's degrees are widely accepted for higher education, employment, and
-                            academic equivalence across institutions.</p>
+                        <div class="approval-icon"><img src="images/aacsb-new.webp" alt="AICTE Approved"></div>
+                        <h4>AACSB</h4>
+                        <p>The approval association signals a global business-quality benchmark, strengthening
+                            leadership and learning value.</p>
                     </div>
                 </div>
             </div>
